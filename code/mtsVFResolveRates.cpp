56954adfc984314423b7ef964a80c6b027e49e5d
Preetham Chalasani <pchalas1@hotmail.com>
2014-12-20 15:24:06 -0500

    In progress: Implementing resolved rates algo

--- code/mtsVFResolveRates.cpp
+++ code/mtsVFResolveRates.cpp
@@ -36,6 +36,38 @@ void mtsVFResolveRates::FillInTableauRefs(const CONTROLLERMODE mode, const doubl
     vctFrm4x4 DesiredFrame;
     DesiredFrame.FromNormalized(DesiredKinematics->Frame);
 
+    // Incremental Frame
+    vctFrm4x4 Pose_dx = CurrentFrame.Inverse() * DesiredFrame;
+
+    // Current position error
+    vctDoubleVec ep;
+    ep.Assign(DesiredFrame.Translation() - CurrentFrame.Translation());
+
+    // Known variables
+    vctDoubleVec vMin(3,0.0);
+    vMin = ep.Normalized();
+    double epsilon_p, alpha, lambda, vMax;
+    alpha = (lambda * epsilon_p)/ vMax;
+
+    // Translation Part
+    vct3 dx_translation = (ep-vMin).Multiply((signbit(ep.Norm() - epsilon_p)));
+
+    // Rotation Part
+    vct3 dx_rotation;
+    vctAxAnRot3 dxRot;
+    vct3 dxRotVec;
+    dxRot.FromNormalized(Pose_dx.Rotation());
+    dxRotVec = dxRot.Axis() * dxRot.Angle();
+    dx_rotation[0] = dxRotVec[0];
+    dx_rotation[1] = dxRotVec[1];
+    dx_rotation[2] = dxRotVec[2];
+    dx_rotation = CurrentFrame.Rotation() * dx_rotation;
+
+    vctDoubleVec dx(6);
+    std::copy(dx_translation.begin(), dx_translation.end(), dx.begin()  );
+    std::copy(dx_rotation.begin()   , dx_rotation.end()   , dx.begin()+3);
+    ObjectiveVectorRef.Assign(dx);
+
     // Put Jacobian into matrix ref
     ObjectiveMatrixRef.Zeros();
     ObjectiveMatrixRef.Diagonal().SetAll(1.0);
